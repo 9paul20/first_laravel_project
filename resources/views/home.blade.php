f229a757ab5463224b831a0e8ad602ade057dd47
Paul Valdez <18030566@itcelaya.edu.mx>
2023-02-02 14:35:26 -0600

    Inicio del proyecto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="/css/app.css">
+<script src="/js/app.js" defer></script>    
+<h1>Nothing</h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo corrupti sequi rem autem repellat unde alias. Natus quaerat doloremque molestias, dolores aperiam delectus possimus est cumque optio excepturi necessitatibus earum?
